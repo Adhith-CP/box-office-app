37371d2d82f572271678de98b3c0b44e92e4e68a
Adhith CP <cpadhith@gmail.com>
2023-06-27 12:48:25 +0530

    added text box for searching , search for shiws on buton click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
-  return <Link to="/starred">Go to starred page</Link>;
+  const [searchStr, setSearchStr] = useState("");
+  const onSearchInputChange = (e) => {
+    setSearchStr(e.target.value);
+  };
+
+  const onSearch = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = response.json;
+    console.log(body);
+
+    // https://api.tvmaze.com/search/shows?q=girls
+  };
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
