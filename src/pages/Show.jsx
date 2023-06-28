ce9310e007e3784f34d0c457f05c9034404eac93
Adhith CP <cpadhith@gmail.com>
2023-06-28 14:35:28 +0530

    created dynamic show page

---
+++ src/pages/Show.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+
+
+const Show = () => {
+
+    const {showId} = useParams()
+
+  return <div>Show page{showId}</div>;
+};
+
+export default Show;
